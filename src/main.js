3d13089b334ca167578b2c21672842bb17caf142
M. Esat Akpunar <akpunar@reybex.com>
2024-03-17 18:44:02 +0300

    Add Vuex store and axios for store management

--- src/main.js
+++ src/main.js
@@ -4,18 +4,18 @@ import '@/assets/styles.css'
 import '@/assets/flags.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import Toast from 'primevue/toast';
 
 import App from './modules/app/App.vue'
 import router from './router'
+import store from './store';
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
+app.use(store)
 app.use(PrimeVue)
 app.component('Toast', Toast)
 app.use(ToastService)
