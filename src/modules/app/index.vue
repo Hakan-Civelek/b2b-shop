11f1a2e82fc454be987c6bbb64ec8c8a8b96e05e
Esat Akpunar <esatakpunar@gmail.com>
2024-04-08 00:10:57 +0300

    Refactor isLoading logic in App component

--- src/modules/app/index.vue
+++ src/modules/app/index.vue
@@ -1,20 +1,36 @@
 <script>
 import ShopToolbar from '@/shared/ShopToolbar.vue'
+import { mapActions } from 'vuex'
 export default {
   name: 'App',
   components: {
     ShopToolbar
   },
+  data() {
+    return {
+      isLoading: false
+    }
+  },
   computed: {
     isNoLayoutPage() {
       return this.$route.meta.noLayout
     }
+  },
+  created() {
+    this.isLoading = true
+    this.getLoggedInUser()
+      .then(() => {
+        this.isLoading = false
+      })
+  },
+  methods: {
+    ...mapActions('app', ['getLoggedInUser'])
   }
 }
 </script>
 
 <template>
-  <ShopToolbar v-if="!isNoLayoutPage" />
+  <ShopToolbar v-if="!isNoLayoutPage && !isLoading" />
   <Toast position="bottom-right" group="br" />
-  <router-view />
+  <router-view v-if="!isLoading" />
 </template>
